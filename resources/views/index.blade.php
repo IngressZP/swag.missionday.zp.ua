f9bf214d111ccdd2ff1bb48e05bc4ed9160b6aab
Kristobal <junta.kristobal@gmail.com>
2018-04-03 21:55:57 +0300

    set up webpack

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 
-    {{--<link rel="stylesheet" href="/css/main.css">--}}
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
     <h1>Hello</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,44 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
-    entry: "./resources/assets/js/index.js",
-    output: {
-        path: path.resolve(__dirname, "public/js"),
-        filename: "index.js"
+    mode: "development",
+    entry: {
+        "./js/index": "./resources/assets/js/index.js",
+        "./css/main": "./resources/assets/scss/main.scss"
     },
-    mode: "development"
+    output: {
+        path: path.resolve(__dirname, "public")
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            }
+        ]
+    }
 };
