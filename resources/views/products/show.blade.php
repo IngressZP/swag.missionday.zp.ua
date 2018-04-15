4e446fd5a89ee1cf64e1db3d204c6a2dc1130430
kaminarifox <kaminarifox@gmail.com>
2018-04-15 20:21:50 +0300

    product page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.master')
 
 @section('content')
-  @include('blocks.header')
+    @include('blocks.header')
+        <div class="container">
+            <div class="row">
+                <div class="col-lg">
+                  <div class="product__name">
+                      {{ $product->name }}
+                  </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
 
-  {{ $product->name }}
+                </div>
+                <div class="col-lg">
+                    <div class="class row">
+                        <div class="col-lg-12">
+                            <div class="product__price">
+                                {{ uah($product->price) }}
+                            </div>
+                        </div>
+                        <div class="d-flex col-lg-12">
+                            <div class="product__size">
+                                <span>S</span>
+                            </div>
+                            <div class="product__size product__size_active">
+                                <span>M</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
 @endsection
