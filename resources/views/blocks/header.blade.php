5676c8a085704a22b88407d732c815db3d81d805
Kristobal <junta.kristobal@gmail.com>
2018-04-16 00:15:12 +0300

    updated header links

--- resources/views/blocks/header.blade.php
+++ resources/views/blocks/header.blade.php
@@ -9,10 +9,13 @@
       </div>
       <div class="header-info">
         <div class="header-info-contacts">
-          <a href="https://t.me/missiondayzpchat" target="__blank"><i class="fab fa-telegram"></i></a>
-          <a href="mailto:info@missionday.zp.ua" target="__blank"><i class="fas fa-at"></i></a>
-          <a href="https://www.instagram.com/missiondayzp/" target="__blank"><i class="fab fa-instagram"></i></a>
-          <a href="https://www.facebook.com/events/166075047320559/" target="__blank"><i class="fab fa-facebook-square"></i></a>
+          <a href="http://events.ingress.com/MissionDay/Zaporizhzhia" target="__blank" rel="noreferrer noopener">
+            <img src="/img/ingress-logo.svg" alt="">
+          </a>
+          <a href="https://t.me/missiondayzpchat" rel="noreferrer noopener" target="__blank"><i class="fab fa-telegram"></i></a>
+          <a href="mailto:info@missionday.zp.ua" rel="noreferrer noopener" target="__blank"><i class="fas fa-at"></i></a>
+          <a href="https://www.instagram.com/missiondayzp/" rel="noreferrer noopener" target="__blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.facebook.com/events/166075047320559/" rel="noreferrer noopener" target="__blank"><i class="fab fa-facebook-square"></i></a>
         </div>
         <div class="header-info__site-link">
           <a href="https://missionday.zp.ua" target="__blank">
